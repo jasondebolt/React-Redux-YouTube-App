7e98f531ad3b82241ed37c7c837c11ded462c75a
Jason DeBolt <jasondebolt@gmail.com>
2017-07-15 15:55:33 -0700

    V2 - YouTube React App, onVideoSelection works

--- src/components/app.js
+++ src/components/app.js
@@ -11,11 +11,17 @@ export default class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {videos: []};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'Tesla Model S'}, (videos) => {
       // Similar to this.setState({videos: videos})
-      this.setState({ videos })
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
     })
   }
 
@@ -23,8 +29,10 @@ export default class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
