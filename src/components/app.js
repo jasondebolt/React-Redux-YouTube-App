76b661e98553e3bf812007350a98c067bddeaaeb
Jason DeBolt <jasondebolt@gmail.com>
2017-07-15 15:45:30 -0700

    V1 - YouTube React App, works pretty well

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './search_bar';
-
-
+import VideoList from './video_list';
+import VideoDetail from './video_detail'
 const API_KEY = 'AIzaSyBG0k35sC72spMm4noocemn1UKimfHOvBs';
 
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'Tesla Model S'}, (videos) => {
+      // Similar to this.setState({videos: videos})
+      this.setState({ videos })
+    })
+  }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
